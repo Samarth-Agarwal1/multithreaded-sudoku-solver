de2c1d010194fb987c9563d98450ce8c6e690711
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-10-16 20:55:48 -0400

    Added code that returns valid values for an empty spot in the Sudoku board.

--- solver.c
+++ solver.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #include <time.h>
 
 int get_cell_num();
 int solve(int, int **);
+int *get_allowed_vals(int, int, int **, int *);
 
-#define BOARD_LENGTH 9
+#define BOARD_LENGTH 4
 
 int main() {
     int **board = (int **)(malloc(BOARD_LENGTH * sizeof(int *)));
@@ -52,8 +55,56 @@ int solve(int pos, int **board) {
     }
     else if (board[y][x] == 0)
     {
+        int num_allowed_vals;
+        int *allowed_vals = get_allowed_vals(x, y, board, &num_allowed_vals);
+
+        if (num_allowed_vals == 0) {
+            return -1;
+        }
         return 0;
     }
+    else
+    {
+        return solve(pos + 1, board);
+    }
+}
 
-    return 0;
+int *get_allowed_vals(int x, int y, int **board, int *num_allowed_vals) {
+    int *allowed_vals = malloc(sizeof(int) * BOARD_LENGTH);
+    *num_allowed_vals = 0;
+    int val;
+    for (val = 1; val <= BOARD_LENGTH; val++) {
+        bool allowed_val = true;
+        int i;
+        for (i = 0; i < BOARD_LENGTH; i++) {
+            if (board[y][i] == val) {
+                allowed_val = false;
+            }
+            else if (board[i][x] == val)
+            {
+                allowed_val = false;
+            }
+        }
+
+        int sub_square_length = (int) sqrt(BOARD_LENGTH);
+        int sub_square_x = (x / sub_square_length) * sub_square_length;
+        int sub_square_y = (y / sub_square_length) * sub_square_length;
+        for (i = sub_square_y; i < (sub_square_y + sub_square_length); i++)
+        {
+            int j;
+            for (j = sub_square_x; j < (sub_square_x + sub_square_length); j++)
+            {
+                if (board[i][j] == val) {
+                    allowed_val = false;
+                }
+            }
+        }
+
+        if (allowed_val) {
+            allowed_vals[*num_allowed_vals] = val;
+            *num_allowed_vals += 1;
+        }
+    }
+
+    return allowed_vals;
 }
